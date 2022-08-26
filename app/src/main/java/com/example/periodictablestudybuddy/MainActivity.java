47a2361dcc7d1bfeec7bf1211551e56c4e349b7d
francistopher <francistopherrr@outlook.com>
2022-08-26 13:02:28 -0400

    CREATE Colors.java UPDATE AndroidManifest.xml MainActivity.java

--- app/src/main/java/com/example/periodictablestudybuddy/MainActivity.java
+++ app/src/main/java/com/example/periodictablestudybuddy/MainActivity.java
@@ -6,21 +6,19 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.periodictablestudybuddy.Utilities.Colors;
 import com.example.periodictablestudybuddy.Utilities.Dimensions;
 
 public class MainActivity extends AppCompatActivity {
-
-    private void setDimensionsInstance() {
-        Dimensions.setInstance(getWindowManager());
-        Dimensions instance = Dimensions.getInstance();
-        //System.out.println("Width " + instance.getWidth(0.5f) + ", Height: " + instance.getHeight(0.5f));
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         View mainLayout = findViewById(R.id.laidout);
-        mainLayout.setBackgroundColor(Color.BLACK);
+        mainLayout.setBackgroundColor(Colors.TEAL_LIGHT);
+
+        Dimensions.setInstance(getWindowManager());
+
     }
 }
---
+++ app/src/main/java/com/example/periodictablestudybuddy/Utilities/Colors.java
@@ -0,0 +1,8 @@
+package com.example.periodictablestudybuddy.Utilities;
+
+import android.graphics.Color;
+
+public class Colors {
+    public static final int TEAL_LIGHT = Color.rgb(48, 176, 199);
+    public static final int TEAL_DARK = Color.rgb(64, 200, 224);
+}
